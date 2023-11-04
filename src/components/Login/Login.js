1e10264b01271e2d4f36d34af5aa09ddaf846104
maq-ahmad-20 <Maqboolahmad081@gmail.com>
2023-11-04 16:19:01 +0530

    usingContextApi

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -15,6 +15,17 @@ const emailReducer = (state, action) => {
 
   return { value: "", isValid: false }
 }
+const collegeReducer = (state, action) => {
+  if (action.type === 'USER_INPUT') {
+
+    return { value: action.val, isValid: action.val.trim().length > 3 }
+  }
+  if (action.type === 'USER_BLUR') {
+    return { value: state.value, isValid: state.value.trim().length > 3 }
+  }
+
+  return { value: "", isValid: false }
+}
 
 const passwordReducer = (state, action) => {
   if (action.type === 'Password_input') {
@@ -33,37 +44,39 @@ const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
   // const [enteredPassword, setEnteredPassword] = useState('');
-  const [enteredColllege, setEnteredCollege] = useState('');
-  const [collegeIsValid, setCollegeIsValid] = useState()
+  // const [enteredColllege, setEnteredCollege] = useState('');
+  // const [collegeIsValid, setCollegeIsValid] = useState()
   // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, { value: '', isValid: null })
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: '', isValid: null })
+  const [collegeState, dispatchCollege] = useReducer(collegeReducer, { value: '', isValid: null })
 
-  // useEffect(() => {
-  //   const identifier = setTimeout(() => {
-  //     setFormIsValid(
-  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6 && enteredColllege.trim().length > 3
-  //     );
+  const { isValid: emailIsValid } = emailState
+  const { isValid: passwordIsValid } = passwordState
+  const { isValid: collegeIsValid } = collegeState;
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      setFormIsValid(
+        emailIsValid && passwordIsValid && collegeIsValid
+      );
 
-  //   }, 500);
+    }, 500);
 
-  //   return () => {
-  //     console.log("cleanup");
-  //     clearTimeout(identifier)
-  //   };
-  // }, [enteredEmail, enteredPassword, enteredColllege])
+    return () => {
+      console.log("cleanup");
+      clearTimeout(identifier)
+    };
+  }, [emailIsValid, passwordIsValid, collegeIsValid])
 
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: 'USER_INPUT', val: event.target.value })
 
-    setFormIsValid(event.target.value.includes('@') && passwordState.isValid);
-
-
   };
   const collegeChangeHandler = (event) => {
-    setEnteredCollege(event.target.value);
+
+    dispatchCollege({ type: 'USER_INPUT', val: event.target.value })
 
 
   };
@@ -72,9 +85,7 @@ const Login = (props) => {
 
     dispatchPassword({ type: "Password_input", val: event.target.value })
 
-    setFormIsValid(
-      event.target.value.trim().length > 6 && emailState.isValid
-    );
+
   };
 
   const validateEmailHandler = () => {
@@ -85,13 +96,13 @@ const Login = (props) => {
     dispatchPassword({ type: "Password_validate" })
   };
   const validateCollegeHandler = () => {
-    setCollegeIsValid(enteredColllege.trim().length > 3);
+    dispatchCollege({ type: 'INPUT_BLUR' })
   };
 
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value, enteredColllege);
+    props.onLogin(emailState.value, passwordState.value, collegeState.value);
   };
 
   return (
@@ -126,14 +137,14 @@ const Login = (props) => {
         </div>
 
         <div
-          className={`${classes.control} ${collegeIsValid === false ? classes.invalid : ''
+          className={`${classes.control} ${collegeState.isValid === false ? classes.invalid : ''
             }`}
         >
           <label htmlFor="college">College</label>
           <input
             type="text"
             id="college"
-            value={enteredColllege}
+            value={collegeState.value}
             onChange={collegeChangeHandler}
             onBlur={validateCollegeHandler}
           />
